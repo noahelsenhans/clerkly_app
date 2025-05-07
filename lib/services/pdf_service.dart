b68651d65584980a6cf470dd819ac32b4553eff2
Noah Elsenhans <noah.elsenhansck@gmail.com>
2025-05-07 18:35:37 +0200

    ✨ PDF Service hinzugefügt

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -1,25 +1,33 @@
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:pdf/pdf.dart';
 
 class PDFService {
   static Future<File> createPDFfromImage(File imageFile, String fileName) async {
     final pdf = pw.Document();
-    final image = pw.MemoryImage(await imageFile.readAsBytes());
+
+    final image = pw.MemoryImage(
+      await imageFile.readAsBytes(),
+    );
 
     pdf.addPage(
       pw.Page(
-        build: (pw.Context context) => pw.Center(
-          child: pw.Image(image),
-        ),
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Image(image),
+          );
+        },
       ),
     );
 
     final outputDir = await getApplicationDocumentsDirectory();
-    final file = File('${outputDir.path}/$fileName.pdf');
-    await file.writeAsBytes(await pdf.save());
+    final pdfFile = File('${outputDir.path}/$fileName.pdf');
 
-    return file;
+    await pdfFile.writeAsBytes(await pdf.save());
+
+    return pdfFile;
   }
 }
 
